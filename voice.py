105d06f95ccca79a504bcf83645c93388c377e68
samuelcomp <samuelmulugeta3@gmail.com>
2021-05-03 13:25:38 +0300

    ok this is testing

--- voice.py
+++ voice.py
@@ -1,9 +1,9 @@
 import speech_recognition as sr
 r = sr.Recognizer()
 with sr.Microphone() as source:
-    print ('Say Something!')
+    print ('Say Something!!!')
     audio = r.listen(source)
-    print ('Done!')
+    print ('Done!!!')
     
 text = r.recognize_google(audio)
 print (text)
